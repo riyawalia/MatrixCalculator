777f2deb858ae97ebc8be2997a9fce15c5f40cd2
Riya Walia <riyawalia12@gmail.com>
2017-06-17 01:14:42 -0400

    Loads data into matrix objects

--- MatrixCalculator/MatrixCalculator/TextFile.cs
+++ MatrixCalculator/MatrixCalculator/TextFile.cs
@@ -1,14 +1,55 @@
 ﻿using System;
-namespace MatrxCalculator
+using System.Configuration;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MatrixCalculator
 {
 	public class TextFile
 	{
 		public Functions functions;
-        public string fileName; 
+        public string fileDirectory; 
+        public string filePath = null;
+        public List<string> rawText; 
         // file path is saved in app config, user can select while file to use 
+        protected bool EnterFileName()
+        {
+			Console.WriteLine("Enter file name:");
+			string fileName = Console.ReadLine();
+
+            if (fileName == "M")
+            {
+                return false; 
+            }
+            this.filePath = this.fileDirectory + "\\" + fileName;
+            return true; 
+		}
 		public TextFile()
 		{
-			// file handling 
+            this.fileDirectory = ConfigurationManager.AppSettings["fileDirectory"];
+            string[] matrixSeperator = { "" }; 
+            if (!Directory.Exists(fileDirectory))
+            {
+                Console.WriteLine("Directory does not exist. Go to App Config to change file directory");
+                return; 
+            }
+            EnterFileName(); 
+            while(!File.Exists(this.filePath))
+            {
+				Console.WriteLine("Invalid file name! Try again. Enter M to exit to Main Menu.");
+                if (!EnterFileName())
+                {
+                    return; 
+                }
+			}
+            foreach (string line in File.ReadLines(this.filePath))
+            {
+                this.rawText.Add(line); 
+            }
+
+            this.functions.leftMatrix.SetValues(this.rawText[0]);
+            this.functions.rightMatrix.SetValues(this.rawText[1]); 
+             
 			// prepare for error if format of text file is unexpected
 		}
 	}
